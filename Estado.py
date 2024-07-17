75399a777594a882281cde21974b44eb436efd4f
Bruna Lucas <151275054+BrunaLucad2004@users.noreply.github.com>
2024-07-16 20:25:32 -0400

    Update Estado.py

--- Estado.py
+++ Estado.py
@@ -3,6 +3,10 @@ import Heap
 #Configuração Final como variável global
 estado_final = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
+#Dicionario com as direções
+direcoes = { 'W': (-1, 0), 'S': (1, 0), 'A': (0, -1), 'D': (0, 1)}
+#             W -> cima     S -> baixo   A-> esquerda  D -> direita
+
 class Estado:
     def __init__(self, configuracao):
         #Controi uma configuração
@@ -11,9 +15,11 @@ class Estado:
         self.heuristica = self.calcular_heuristica()
 
     def __lt__(self, other):
+        #Vai ordenar pela menor heuristica 
         return self.heuristica < other.heuristica
 
     def __repr__(self):
+        #Definindo a representação da "matriz" do puzzel
         return '\n'.join([' '.join(' ' if item == 0 else str(item) for item in linha) for linha in self.configuracao])
 
     #Esta função calcula a Heuristica de uma configuração, ou seja, quantas peças estão fora do lugar, exceto o '0'
@@ -25,39 +31,52 @@ class Estado:
                     heuristica += 1
         return heuristica
     
+    def encontrar_zero(self):
+        # Itera sobre cada lista em self.configuracao com seus índices
+        for i, lista in enumerate(self.configuracao):
+            # Verifica se 0 está na lista
+            if 0 in lista:
+                # Retorna a posição do zero
+                return i, lista.index(0)
+        return None, None
+
+    
+    def gerar_novo_estado(self, linha, coluna, nova_linha, nova_coluna):
+        # Cria um outro estado a partir do atual
+        novo_estado = []
+        for linha_atual in self.configuracao:
+            nova_linha_atual = linha_atual[:]
+            novo_estado.append(nova_linha_atual)
+        
+        # Troca valores
+        novo_estado[linha][coluna], novo_estado[nova_linha][nova_coluna] = novo_estado[nova_linha][nova_coluna], novo_estado[linha][coluna]
+        
+        return novo_estado
+    
+
     #Esta função gera todos os estados possíveis que podem ser alcançados a partir do estado atual
     def transicoes(self):
         transicoes = []
-        linha, coluna = next((i, sublist.index(0)) for i, sublist in enumerate(self.configuracao) if 0 in sublist)
-        
-        direcoes = {
-            'W': (-1, 0), # W -> cima
-            'S': (1, 0),  # S -> baixo
-            'A': (0, -1), # A -> esquerda
-            'D': (0, 1)   # D -> direita
-        }
-        
+
+        # Verifica se o valor '0' está na configuração
+        linha, coluna = self.encontrar_zero()
+
         for direcao, (x, y) in direcoes.items():
             nova_linha = linha + x
             nova_coluna = coluna + y
-            if 0 <= nova_linha < len(self.configuracao) and 0 <= nova_coluna < len(self.configuracao[0]):
-                novo_estado = [linha[:] for linha in self.configuracao]
-                novo_estado[linha][coluna], novo_estado[nova_linha][nova_coluna] = novo_estado[nova_linha][nova_coluna], novo_estado[linha][coluna]
+            if ((0 <= nova_linha) and (nova_linha < len(self.configuracao))) and ((0 <= nova_coluna) and (nova_coluna < len(self.configuracao[0]))):
+                # Gera um novo estado usando a função modularizada
+                novo_estado = self.gerar_novo_estado(linha, coluna, nova_linha, nova_coluna)
+                #adiciona essa nova configuração a lista de transições, junto com o movimento que a tornou possivel
                 transicoes.append((Estado(novo_estado), direcao))
         
         return transicoes
 
     #Esta função gera todos os movimentos possiveis a partir do estado atual
     def movimentos_possiveis(self):
-         # Verifica se o valor '0' está na configuração
-        linha, coluna = next((i, sublist.index(0)) for i, sublist in enumerate(self.configuracao) if 0 in sublist)
+        # Verifica se o valor '0' está na configuração
+        linha, coluna = self.encontrar_zero()
         
-        direcoes = {
-            'W': (-1, 0), # W -> cima
-            'S': (1, 0),  # S -> baixo
-            'A': (0, -1), # A -> esquerda
-            'D': (0, 1)   # D -> direita
-        }
         movimentos = []
         for direcao, (x, y) in direcoes.items():
             nova_linha = linha + x
@@ -68,24 +87,20 @@ class Estado:
 
     #Esta função realiza um movimento e gera um novo estado
     def realizar_movimento(self, movimento):
-        linha, coluna = next((i, sublist.index(0)) for i, sublist in enumerate(self.configuracao) if 0 in sublist)
-
-        direcoes = {
-            'W': (-1, 0), # W -> cima
-            'S': (1, 0),  # S -> baixo
-            'A': (0, -1), # A -> esquerda
-            'D': (0, 1)   # D -> direita
-        }
+        # Verifica se o valor '0' está na configuração
+        linha, coluna = self.encontrar_zero()
 
         if movimento in direcoes:
             x, y = direcoes[movimento]
             nova_linha = linha + x
             nova_coluna = coluna + y
             if (0 <= nova_linha < 3) and (0 <= nova_coluna < 3):
-                novo_estado = [linha[:] for linha in self.configuracao]
-                novo_estado[linha][coluna], novo_estado[nova_linha][nova_coluna] = novo_estado[nova_linha][nova_coluna], novo_estado[linha][coluna]
+                # Gera um novo estado usando a função modularizada
+                novo_estado = self.gerar_novo_estado(linha, coluna, nova_linha, nova_coluna)
             return Estado(novo_estado)
         return self
+
+    #CONTINUAR INSPEÇÃO DAQUI
 
     def reconstruir_caminho(veio_de, movimentos, atual):
         caminho = []
@@ -132,3 +147,4 @@ class Estado:
         else:
             print("Nenhum caminho encontrado.")
 
+#verificar se concluiu o jogo
